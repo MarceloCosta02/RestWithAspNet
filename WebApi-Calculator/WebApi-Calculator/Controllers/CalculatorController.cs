384e7aee82111a68f1b70616767e62fe1ea30289
Marcelo De Lucca Costa Da Ponte <marcelo.ponte@metalsa.com>
2020-05-08 19:36:11 -0300

    Operação de Soma realizada

---
+++ WebApi-Calculator/WebApi-Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/calculator/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+
+            // Tenta passar para double, com os System.Globalization 
+            //gerenciando todos os formatos de numeros possíveis 
+            bool isNumber = double.TryParse(strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number);
+
+            return isNumber;
+        }
+    }
+}
