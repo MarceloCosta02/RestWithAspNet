ef7d7b294388d9c130bfa817a7937d70a909437b
Marcelo De Lucca Costa Da Ponte <marcelo.ponte@metalsa.com>
2020-05-13 16:24:25 -0300

    Adicionado o Data Contract, que se torna possível ordenar e renomear os campos na respota das requisições

--- WebApi-Persons/WebApi-Persons/Data/VO/BookVO.cs
+++ WebApi-Persons/WebApi-Persons/Data/VO/BookVO.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace WebApi_Persons.Data.VO
 {
+    // Com o DataContract, eu posso ordenar e renomear os campos
+    // para aparecerem de maneira personalizada na hora da requisição
+    [DataContract]
     public class BookVO
     {
+        [DataMember(Order = 1, Name = "codigo")]
         public long Id { get; set; }
+
+        [DataMember(Order = 2)]
         public string Title { get; set; }
 
+        [DataMember(Order = 3)]
         public string Author { get; set; }
 
+        [DataMember(Order = 5)]
         public decimal Price { get; set; }
 
+        [DataMember(Order = 4)]
         public DateTime LaunchDate { get; set; }
     }
 }
